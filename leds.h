6c05b862e5972d455be2574bc409a7de590069b3
Richard Barlow <richard@richardbarlow.co.uk>
2011-03-16 19:07:26 +0000

    Add some missing macros into leds.h
    Must have deleted these when I copied the file over from another repo

--- leds.h
+++ leds.h
@@ -7,6 +7,11 @@
 
 #define leds_init() do { P3OUT &= ~LED_DBG; P3DIR |= LED_DBG; } while(0)
 
+#define __led_set(led_out, led, x) do { if (x) led_out |= led; \
+                                        else   led_out &= ~led; } while(0)
+
+#define __led_toggle(led_out, led) do { led_out ^= led; } while(0)
+
 #define dbg_set(x)       __led_set(LED_DBG_OUT, LED_DBG, x)
 #define dbg_toggle()     __led_toggle(LED_DBG_OUT, LED_DBG)
 
